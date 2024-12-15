797015a3f10ac2ddb64a8f105755c3c07e46206b
anwarnagori <ar1498137@gmail.com>
2024-12-15 17:29:38 +0500

    update-code

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -4,10 +4,14 @@ function Pet( props ) {
   const { name, animal, breed, images, location } = props;
   // let item = props.item;
   // console.log(props.item)
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
   return (
     <div className="pet">
       <div className="image-container">
-        <img src={images[0]} alt="" />
+        <img src={hero} alt="" />
       </div>
       <div className="info">
         <h1>{name}</h1>
